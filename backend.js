a190af5f6ece1c32096a104e6e6d8ebdeaf3c370
fredyTheKemy <141065168+fredyTheKemy@users.noreply.github.com>
2024-06-27 14:38:11 +0200

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const app = express();
 
+const port = 5000;  
+
 const corsOptions = {
-    origin:'http://localhost:3000',
+    origin:'http://192.20.10.0:5000',
     methods: 'GET,PUT,POST,DELETE',
     allowedHeaders:'Content-Type,Authorization'
 }
 
+app.use(cors(corsOptions));
+app.use(express.json());
+
 app.get('/test',(req,res) => {
     res.send('<h1>Api is working</h1>')
+})
+
+app.listen(port,()=>{
+    console.log(`app is listening on port ${port}`)
 })
 
